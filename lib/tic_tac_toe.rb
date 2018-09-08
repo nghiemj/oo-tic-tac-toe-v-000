bb3d196fd3ce2c9bddd1391373a35f65561fc050
James Nghiem <nghiem.j@hotmail.com>
2018-09-08 23:25:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,7 @@ end
 
 def won?
   WIN_COMBINATIONS.detect do |combo|
-    @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[2])
+    @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(index, combo[2])
   end
 end
 
