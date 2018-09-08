b6fa53d0c21065203a2f2cf79a7b6f88c5649357
James Nghiem <nghiem.j@hotmail.com>
2018-09-08 23:29:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,4 +97,15 @@ def winner
   end
 end
 
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+  puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+  puts "Cat\'s Game!"
+  end
+end
+
 end
